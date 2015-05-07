6c2d0a53582246d6557349e94028c57cc9f5085b
Kartik Mistry <kartik.mistry@gmail.com>
2015-05-07 16:57:43 +0530

    Imported Upstream version 0.1.0~r60155

---
+++ dev/transliterate/test-compose-intersect.sh
@@ -0,0 +1,1 @@
+hfst-strings2fst  | hfst-compose-intersect -2 lat-kaz.hfst  | hfst-fst2strings 
---
+++ dev/transliterate/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+echo "تاقتايشا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be тақтайша
+
+echo "ايىرماسى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be айырмасы
+
+echo "زەكي" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be зеки
+
+echo "قيناپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be қинап
+
+echo "كوپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be көп
+
+echo "سولتۇستىك" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be солтүстік
+
+echo "تەرموس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be термос
+
+echo "ۇيا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be ұя
+
+echo "مويناق" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+
+echo "كيىم" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be киім
+echo "كيۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be кию
+echo "كيىنىس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be киініс
+
+echo "قويۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be қою
+
+echo "توركىن" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# hrrm
+
+echo "سۇرتكى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be сүрткі
+
+echo "رايون" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be район
+
+echo "بەكەر" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be бекер
+
+echo "پەرىشتە" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be періште
